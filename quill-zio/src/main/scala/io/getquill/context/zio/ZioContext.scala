447bbedb562abbec59ed260b4ae7976af96643c7
Alexander Ioffe <deusaquilus@gmail.com>
2021-03-04 03:44:46 -0500

    Continue refining API

--- quill-zio/src/main/scala/io/getquill/context/zio/ZioContext.scala
+++ quill-zio/src/main/scala/io/getquill/context/zio/ZioContext.scala
@@ -10,20 +10,14 @@ import java.sql.Connection
 
 trait ZioContext[Idiom <: io.getquill.idiom.Idiom, Naming <: NamingStrategy] extends Context[Idiom, Naming]
   with StreamingContext[Idiom, Naming] {
-  import ZioContext._
 
   // It's nice that we don't actually have to import any JDBC libraries to have a Connection type here
-  override type StreamResult[T] = ZStream[BlockingConnection, Throwable, T]
-  override type Result[T] = RIO[BlockingConnection, T]
+  override type StreamResult[T] = ZStream[Has[Session] with Blocking, Throwable, T]
+  override type Result[T] = RIO[Has[Session] with Blocking, T]
   override type RunQueryResult[T] = List[T]
   override type RunQuerySingleResult[T] = T
 
   // Need explicit return-type annotations due to scala/bug#8356. Otherwise macro system will not understand Result[Long]=Task[Long] etc...
-  def executeQuery[T](sql: String, prepare: Prepare = identityPrepare, extractor: Extractor[T] = identityExtractor): RIO[BlockingConnection, List[T]]
-  def executeQuerySingle[T](sql: String, prepare: Prepare = identityPrepare, extractor: Extractor[T] = identityExtractor): RIO[BlockingConnection, T]
-}
-
-object ZioContext {
-  type BlockingConnection = Has[Connection] with Blocking
-  case class Prefix(name: String)
+  def executeQuery[T](sql: String, prepare: Prepare = identityPrepare, extractor: Extractor[T] = identityExtractor): RIO[Has[Session] with Blocking, List[T]]
+  def executeQuerySingle[T](sql: String, prepare: Prepare = identityPrepare, extractor: Extractor[T] = identityExtractor): RIO[Has[Session] with Blocking, T]
 }
