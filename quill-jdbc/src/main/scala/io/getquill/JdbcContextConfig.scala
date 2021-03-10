7efd3546e65c44bc7050f24587df661fca5327ec
Alexander Ioffe <deusaquilus@gmail.com>
2021-03-10 00:44:45 -0500

    Example app

--- quill-jdbc/src/main/scala/io/getquill/JdbcContextConfig.scala
+++ quill-jdbc/src/main/scala/io/getquill/JdbcContextConfig.scala
@@ -4,9 +4,7 @@ import com.typesafe.config.Config
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 
-import java.io.Closeable
 import java.util.Properties
-import javax.sql.DataSource
 import scala.util.control.NonFatal
 
 case class JdbcContextConfig(config: Config) {
